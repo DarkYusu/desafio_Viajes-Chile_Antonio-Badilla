72395716a087fd51ff9f60ea0262a2b5230f8c21
Antonio Badilla <abadilla2009@gmail.com>
2023-04-24 21:24:15 -0400

    Agregado comentarios al JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
+//JS de tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
+//Alerta de mensaje enviado
 $(document).ready(function () {
     $("#botonEnviar").click(function () {
       alert("El mensaje fue enviado correctamente...");
